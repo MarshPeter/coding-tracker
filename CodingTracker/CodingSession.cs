e5747401b2f572efbb3c6d486525aaa628f0ff4e
Peter Farmer <peter.m.farmer.204@gmail.com>
2023-10-27 16:54:57 +1100

    CodingSession Model added

---
+++ CodingTracker/CodingSession.cs
@@ -0,0 +1,11 @@
+namespace CodingTracker;
+
+class CodingSession 
+{
+
+    public int Id { get; set; }
+    public string? StartTime { get; set; }
+    public string? EndTime { get; set; }
+    public double Duration { get; set; }
+
+}
---
+++ CodingTracker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using Microsoft.Data.Sqlite;
+
+namespace CodingTracker;
+class Program
+{
+    static string connectionString = @"Data source=coding-tracker.db";
+
+    static void Main(string[] args) 
+    {
+        string test1;
+        test1 = ConfigurationManager.AppSettings.Get("connection-string")!;
+        Console.WriteLine(test1);
+
+        // using (var connection = new SqliteConnection(connectionString)) {
+        //     connection.Open();
+        //     var tableCmd = connection.CreateCommand();
+
+        //     tableCmd.CommandText = 
+        //     @"CREATE TABLE IF NOT EXISTS";
+
+        //     tableCmd.ExecuteNonQuery();
+
+        //     connection.Close();
+        // }
+    }
+}
